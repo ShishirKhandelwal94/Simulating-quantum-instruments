d9c12a51c20143173a87f0e374d59cfebd328495
Shishir <shishirkhandelwal94@gmail.com>
2025-02-27 16:58:26 +0100

    sequential_chsh.m

--- sequential_chsh.m
+++ sequential_chsh.m
@@ -17,10 +17,10 @@ X=[0 1;1 0];Y=[0 -i;i 0]; Z=[1 0;0 -1]; id=eye(2);
 dat=[];
 T=[1 1; 1 -1];
 
-alpha=2.05;
+for alpha=[2.13:0.01:2.13];
 
-ll=[];
-for repeat=1:1
+    ll=[];
+for repeat=1:50
     
 for x=1:2
         sigma(:,:,1,x)=RandomDensityMatrix(2,1,1)/2;
@@ -32,17 +32,11 @@ M(:,:,1)=X;%RandomDensityMatrix(2,0,1);%(X+Z)/sqrt(2);
 M(:,:,2)=Z;%RandomDensityMatrix(2,0,1);%(X-Z)/sqrt(2);
 
 
-% obj=0;
-% for x=1:4
-%     for y=1:2
-%         obj=obj+T(x,y)*trace(rho(:,:,x)*M(:,:,y));
-%     end
-% end
 
 
 
 
-for K=1:200
+for K=1:100
 
 
     
@@ -77,15 +71,10 @@ PartialTrace(y2(:,:,2),1)==trace(y2(:,:,2))*eye(2)/2;
 trace(x1(:,:,1)+x1(:,:,2)+y1(:,:,1)+y1(:,:,2))==1;
 trace(x2(:,:,1)+x2(:,:,2)+y2(:,:,1)+y2(:,:,2))==1;
 
-
-
-%%%%%%%%% unital channel %%%%%%%%%
-PartialTrace(x1(:,:,1)+x1(:,:,2),2)==trace(x1(:,:,1)+x1(:,:,2))*eye(2)/2;
-PartialTrace(x2(:,:,1)+x2(:,:,2),2)==trace(x2(:,:,1)+x2(:,:,2))*eye(2)/2;
-PartialTrace(y1(:,:,1),2)==trace(y1(:,:,1))*eye(2)/2;
-PartialTrace(y1(:,:,2),2)==trace(y1(:,:,2))*eye(2)/2;
-PartialTrace(y2(:,:,1),2)==trace(y2(:,:,1))*eye(2)/2;
-PartialTrace(y2(:,:,2),2)==trace(y2(:,:,2))*eye(2)/2;
+trace(x1(:,:,1))==trace(x1(:,:,1)+x1(:,:,2))*1/2;
+trace(x1(:,:,2))==trace(x1(:,:,1)+x1(:,:,2))*1/2;
+trace(x2(:,:,1))==trace(x2(:,:,1)+x2(:,:,2))*1/2;
+trace(x2(:,:,2))==trace(x2(:,:,1)+x2(:,:,2))*1/2;
 
 
 
@@ -141,7 +130,8 @@ T0=sigma(:,:,1,1)+sigma(:,:,2,1);
 sigma(:,:,1,1)+sigma(:,:,2,1)==sigma(:,:,1,2)+sigma(:,:,2,2);
 trace(sigma(:,:,1,1)+sigma(:,:,2,1))==1;
 
-T0(1,2)==0;
+
+
 
 
 for x=1:2
@@ -218,7 +208,7 @@ S1>=alpha;
 
 maximise(real(S2))
 cvx_end
-cvx_optval
+cvx_optval;
 end
 
 
@@ -226,14 +216,22 @@ ll=[ll, cvx_optval]
 end
 
 
+dat=[dat; alpha max(ll)]
+end
 
 
 
 
 
+%% 
 
-
-
+tau111=x2(:,:,1);
+tau211=x2(:,:,2);
+tau120=y2(:,:,1);
+tau202=y2(:,:,2);
+q11=trace(x2(:,:,1)+x2(:,:,2));
+q20=trace(y2(:,:,1));
+q02=trace(y2(:,:,2));
 
 
 
