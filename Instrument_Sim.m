e350c0d1ea78640b5de44fcf682ca7a802b5522c
Shishir <shishirkhandelwal94@gmail.com>
2025-02-27 16:57:26 +0100

    Instrument_Sim.m

--- Instrument_Sim.m
+++ Instrument_Sim.m
@@ -3,59 +3,30 @@
 clc
 clearvars
 
-N=3; %Specify number of outcomes
-d=3; %Specify dimension
+N=2; %Specify number of outcomes
+d=2; %Specify dimension
 plist=genpart(N,d); %Generates a list of all unique rank vectors, see genpart function
 Nr=size(plist,1); %Gives the total number of unique rank vectors
 
 %Instrument Choi matrices need to be specified as a list eta(:,:,a), where a is an outcome
 %Example: below we give the Choi matrices for weak Z in three dimensions
 
-gamma = 0.5; %Degree of sharpness
-eta(:,:,1) = [
-    (1 + gamma) / 6, 0, 0, 0, sqrt(1 - gamma^2) / (6 * sqrt(2)), 0, 0, 0, sqrt(1 - gamma^2) / (6 * sqrt(2));
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    sqrt(1 - gamma^2) / (6 * sqrt(2)), 0, 0, 0, (1 - gamma) / 12, 0, 0, 0, (1 - gamma) / 12;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    sqrt(1 - gamma^2) / (6 * sqrt(2)), 0, 0, 0, (1 - gamma) / 12, 0, 0, 0, (1 - gamma) / 12
-];
+gamma = 0.9; %Degree of sharpness
 
-eta(:,:,2) = [
-    (1 - gamma) / 12, 0, 0, 0, sqrt(1 - gamma^2) / (6 * sqrt(2)), 0, 0, 0, (1 - gamma) / 12;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    sqrt(1 - gamma^2) / (6 * sqrt(2)), 0, 0, 0, (1 + gamma) / 6, 0, 0, 0, sqrt(1 - gamma^2) / (6 * sqrt(2));
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    (1 - gamma) / 12, 0, 0, 0, sqrt(1 - gamma^2) / (6 * sqrt(2)), 0, 0, 0, (1 - gamma) / 12
-];
-
-eta(:,:,3) = [
-    (1 - gamma) / 12, 0, 0, 0, (1 - gamma) / 12, 0, 0, 0, sqrt(1 - gamma^2) / (6 * sqrt(2));
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    (1 - gamma) / 12, 0, 0, 0, (1 - gamma) / 12, 0, 0, 0, sqrt(1 - gamma^2) / (6 * sqrt(2));
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    sqrt(1 - gamma^2) / (6 * sqrt(2)), 0, 0, 0, sqrt(1 - gamma^2) / (6 * sqrt(2)), 0, 0, 0, (1 + gamma) / 6
-];
+for i=1:4
+    eta(:,:,i) = eta_a(i,d,gamma); %Defines weak Z Chois using the eta_a function
+end
 
 % Select type type of noise
 
-noise = eye(d^2)/(d^2*N); %White noise
-%noise = dephasing_noise(d,N);  %Dephasing noise, see dephasing_noise function
+%noise = eye(d^2)/(d^2*N); %White noise
+noise = dephasing_noise(d,N);  %Dephasing noise, see dephasing_noise function
 
 
 
 cvx_begin sdp
+cvx_solver sedumi
+cvx_precision best
 
     variable X(d^2,d^2,Nr,N) hermitian semidefinite %For each element of all rank vectors, we have one X
     %corresponding to a subnormalised Choi for the corresponding element
@@ -74,13 +45,17 @@ for i=1:Nr
     end
 end
 
-%Choi constraint on total Choi states for each rank vector
+%Choi constraint on total Choi states for each rank vector and trace
+%constraint on each X
 for i=1:Nr
     sumX=0;
     for j=1:N
         sumX = sumX + X(:,:,i,j);
     end
     PartialTrace(sumX,1) == trace(sumX)*eye(d)/d;
+    for k=1:N
+        trace(X(:,:,i,k)) == trace(sumX)*plist(i,k)/d;
+    end
 end
 
         
@@ -106,57 +81,27 @@ cvx_end
 clc
 clearvars
 
-N=3; %Specify number of outcomes
-d=3; %Specify dimension
+N=2; %Specify number of outcomes
+d=2; %Specify dimension
 plist=genpart(N,d); %Generates a list of all unique rank vectors, see genpart function
 Nr=size(plist,1); %Gives the total number of unique rank vectors
 
 %Instrument Choi matrices need to be specified as a list eta(:,:,a), where a is an outcome
 %Example: below we give the Choi matrices for weak Z in three dimensions
 
-gamma = 0.5; %Degree of sharpness
-eta(:,:,1) = [
-    (1 + gamma) / 6, 0, 0, 0, sqrt(1 - gamma^2) / (6 * sqrt(2)), 0, 0, 0, sqrt(1 - gamma^2) / (6 * sqrt(2));
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    sqrt(1 - gamma^2) / (6 * sqrt(2)), 0, 0, 0, (1 - gamma) / 12, 0, 0, 0, (1 - gamma) / 12;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    sqrt(1 - gamma^2) / (6 * sqrt(2)), 0, 0, 0, (1 - gamma) / 12, 0, 0, 0, (1 - gamma) / 12
-];
-
-eta(:,:,2) = [
-    (1 - gamma) / 12, 0, 0, 0, sqrt(1 - gamma^2) / (6 * sqrt(2)), 0, 0, 0, (1 - gamma) / 12;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    sqrt(1 - gamma^2) / (6 * sqrt(2)), 0, 0, 0, (1 + gamma) / 6, 0, 0, 0, sqrt(1 - gamma^2) / (6 * sqrt(2));
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    (1 - gamma) / 12, 0, 0, 0, sqrt(1 - gamma^2) / (6 * sqrt(2)), 0, 0, 0, (1 - gamma) / 12
-];
-
-eta(:,:,3) = [
-    (1 - gamma) / 12, 0, 0, 0, (1 - gamma) / 12, 0, 0, 0, sqrt(1 - gamma^2) / (6 * sqrt(2));
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    (1 - gamma) / 12, 0, 0, 0, (1 - gamma) / 12, 0, 0, 0, sqrt(1 - gamma^2) / (6 * sqrt(2));
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    0, 0, 0, 0, 0, 0, 0, 0, 0;
-    sqrt(1 - gamma^2) / (6 * sqrt(2)), 0, 0, 0, sqrt(1 - gamma^2) / (6 * sqrt(2)), 0, 0, 0, (1 + gamma) / 6
-];
+gamma = 0.1; %Degree of sharpness
 
 
 
+for a=1:N
+    eta(:,:,a) = eta_a(a,d,gamma);%Defines weak Z Chois using eta_a function
+end
 
 
 
 cvx_begin sdp
+cvx_solver sedumi
+cvx_precision best
 
     variable X(d^2,d^2,Nr,N) hermitian semidefinite %For each element of all rank vectors, we have one X
     %corresponding to a subnormalised Choi for the corresponding element
@@ -176,24 +121,27 @@ for i=1:Nr
     end
 end
 
-%Choi constraint on total Choi states for each rank vector
+%Choi constraint on total Choi states for each rank vector and trace
+%constraint on each X
 for i=1:Nr
     sumX=0;
     for j=1:N
         sumX = sumX + X(:,:,i,j);
     end
     PartialTrace(sumX,1) == trace(sumX)*eye(d)/d;
+    for k=1:N
+        trace(X(:,:,i,k)) == trace(sumX)*plist(i,k)/d;
+    end
 end
 
 %Noise constraints
-for a=1:N
-    PartialTrace(noise(:,:,a),1)==trace(noise(:,:,a))*eye(d)/d;
-end
+
 snoise=0;
 for a=1:N
     snoise=snoise+noise(:,:,a);
 end
-trace(snoise)==1-v;
+PartialTrace(snoise,1)==(1-v)*eye(d)/d;
+
         
 %Main decomposition constraint
 
@@ -207,7 +155,8 @@ end
 
 %objective
 maximize(v)
-
-         
+        
 
 cvx_end
+
+
